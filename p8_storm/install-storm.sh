90ab65d20037d9043dec3d77798e08e6d1e67290
Kenny Lu <luzhuomi@gmail.com>
2017-12-21 08:11:45 +0800

    updated install-storm.sh

--- p8_storm/install-storm.sh
+++ p8_storm/install-storm.sh
@@ -5,25 +5,20 @@ wget http://www-us.apache.org/dist/storm/apache-storm-1.0.5/apache-storm-1.0.5.t
 tar zxvf apache-storm-1.0.5.tar.gz
 echo "
 storm.zookeeper.servers:
-
-     - \"localhost\"
+     - \"itd353-VirtualBox\"
 
 storm.zookeeper.port: 2181
 
-nimbus.host: \"localhost\"
+nimbus.host: \"itd353-VirtualBox\"
 
 storm.local.dir: \"/mnt/storm\"
 
 java.library.path: \"/usr/local/lib\"
 
 supervisor.slots.ports:
-
      - 6700
-
      - 6701
-
      - 6702
-
      - 6703
 
 worker.childopts: \"-Xmx768m\"
@@ -31,7 +26,14 @@ worker.childopts: \"-Xmx768m\"
 nimbus.childopts: \"-Xmx512m\"
 
 supervisor.childopts: \"-Xmx256m\"
+
+nimbus.seeds: [\"itd353-VirtualBox\"]
+
+storm.zookeeper.root: \"/storm03\"
+
 " >> apache-storm-1.0.5/conf/storm.yaml
 mv apache-storm-1.0.5 /opt/
 chown itd353 -R /opt/apache-storm-1.0.5
+rm -rf /mnt/storm/*
 cd $CURR_DIR
+
